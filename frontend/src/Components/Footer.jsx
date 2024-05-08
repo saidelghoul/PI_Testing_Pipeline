11be50ce649997fe0c3022b7e65d44e15804451e
RamiBH120 <benhmdrami@gmail.com>
2024-05-08 14:19:05 +0100

    updated footer year

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -11,60 +11,58 @@ import "../../public/assets/lib/slick/slick-theme.css";
 
 export default function Footer() {
   return (
-    <>
-      <div className="footy-sec" style={{ backgroundColor: "#e44d3a" }}>
-        <div className="container">
-          <ul>
-            <li>
-              <a href="help-center.html" title="">
-                Help Center
-              </a>
-            </li>
-            <li>
-              <a href="about.html" title="">
-                About
-              </a>
-            </li>
-            <li>
-              <a title="">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="#" title="">
-                Community Guidelines
-              </a>
-            </li>
-            <li>
-              <a href="#" title="">
-                Cookies Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" title="">
-                Career
-              </a>
-            </li>
-            <li>
-              <a href="forum.html" title="">
-                Forum
-              </a>
-            </li>
-            <li>
-              <a href="#" title="">
-                Language
-              </a>
-            </li>
-            <li>
-              <a href="#" title="">
-                Copyright Policy
-              </a>
-            </li>
-          </ul>
-          <p>
-            <img src="images/copy-icon.png" alt="" />
-            Copyright 2019
-          </p>
-        </div>
+    <div className="footy-sec" style={{ backgroundColor: "#e44d3a" }}>
+      <div className="container">
+        <ul>
+          <li>
+            <a href="help-center.html" title="">
+              Help Center
+            </a>
+          </li>
+          <li>
+            <a href="about.html" title="">
+              About
+            </a>
+          </li>
+          <li>
+            <a title="">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#" title="">
+              Community Guidelines
+            </a>
+          </li>
+          <li>
+            <a href="#" title="">
+              Cookies Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" title="">
+              Career
+            </a>
+          </li>
+          <li>
+            <a href="forum.html" title="">
+              Forum
+            </a>
+          </li>
+          <li>
+            <a href="#" title="">
+              Language
+            </a>
+          </li>
+          <li>
+            <a href="#" title="">
+              Copyright Policy
+            </a>
+          </li>
+        </ul>
+        <p>
+          <img src="images/copy-icon.png" alt="" />
+          (C) CodeMasters - 2024
+        </p>
       </div>
-    </>
+    </div>
   );
 }
